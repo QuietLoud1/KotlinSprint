d7b8837f1f7386dd635e4801fc0ced850bba85b2
Feodor <verygood123feodor@gmail.com>
2025-06-27 00:34:45 +0300

    KS-1-2 variables final

--- src/main/kotlin/lesson1_task2.kt
+++ src/main/kotlin/lesson1_task2.kt
@@ -3,7 +3,10 @@ package org.example
 fun main() {
     val orders: Int = 75
     val gratitudeText: String = "Спасибо за покупку в нашем интернет магазине!"
-
+    println(
+        "Заказы: $orders" +
+                "\nБлагодарность: $gratitudeText"
+    )
     var workers = 2000
     // println("Количество работников: $workers")
     workers -= 1
