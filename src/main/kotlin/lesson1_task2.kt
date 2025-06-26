33d05bf306dc0e5f3325daa9985316bbb8794d1e
Feodor <verygood123feodor@gmail.com>
2025-06-27 00:31:42 +0300

    KS-1-2 variables

--- src/main/kotlin/lesson1_task2.kt
+++ src/main/kotlin/lesson1_task2.kt
@@ -1,5 +1,11 @@
 package org.example
 
 fun main() {
+    val orders: Int = 75
+    val gratitudeText: String = "Спасибо за покупку в нашем интернет магазине!"
 
+    var workers = 2000
+    // println("Количество работников: $workers")
+    workers -= 1
+    println("Количество работников: $workers")
 }
