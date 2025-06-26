0589bf7d01ae074d5300f8c2b28c641e4ef2b9f8
Feodor <verygood123feodor@gmail.com>
2025-06-27 00:52:40 +0300

    KS-1-4 variables final

---
+++ src/main/kotlin/lesson1_task4.kt
@@ -0,0 +1,16 @@
+package org.example
+
+fun main() {
+    var length: Long = 40868600000
+    var age: Byte = 27
+    var partOfDay: Double = 0.075
+    var seconds: Short = 6480
+    var partOfYear: Float = 2.0547945205479453E-4f
+    var apogee: Int = 327000
+    println("Расстояние: $length")
+    println("Возраст: $age")
+    println("Часть дня: $partOfDay")
+    println("Секунды: $seconds")
+    println("Часть года: $partOfYear")
+    println("Апогей орбиты: $apogee")
+}
