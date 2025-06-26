cf86ab4dfad98461bf6997124a40c93b6278847e
Feodor <verygood123feodor@gmail.com>
2025-06-27 00:41:21 +0300

    KS-1-3 variables final

---
+++ src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println("Год полета: $year")
+    println("Час полета: $hour")
+    println("Минуты полета: $minute")
+    hour = 10
+    minute = 55
+    println("Время посадки $hour:$minute")
+}
