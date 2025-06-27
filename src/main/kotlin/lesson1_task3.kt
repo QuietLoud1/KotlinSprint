62472aad6c0ba2e5478e4fd06eec68b33f5fc212
Feodor <verygood123feodor@gmail.com>
2025-06-27 17:59:04 +0300

    KS-1-3 fixed variables

--- src/main/kotlin/lesson1_task3.kt
+++ src/main/kotlin/lesson1_task3.kt
@@ -1,13 +1,14 @@
 package org.example
 
 fun main(){
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    // Изменил тип переменных даты на String
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
     println("Год полета: $year")
     println("Час полета: $hour")
     println("Минуты полета: $minute")
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     println("Время посадки $hour:$minute")
 }
